a40dc0a6f16a57fe7d5be281819205dd5fb00899
arsaizdihar <arsadihar@gmail.com>
2022-09-16 20:23:20 +0700

    Create some matrix operators

--- src/lib/Matrix.java
+++ src/lib/Matrix.java
@@ -3,6 +3,8 @@ package lib;
 public class Matrix {
     private double[][] contents;
 
+    /*** KONSTRUKTOR ***/
+
     /**
      * Matriks konstruktor. Menginisiasi matriks dengan ukuran baris x kolom.
      * 
@@ -46,6 +48,20 @@ public class Matrix {
     }
 
     /**
+     * Mengubah baris indeks ke-rowIdx dengan baris baru
+     * <p>
+     * I.S. matriks terdefinisi
+     * <p>
+     * F.S. baris indeks ke-rowIdx diubah dengan baris baru
+     * 
+     * @param rowIdx indeks baris yang akan diubah
+     * @param row    baris baru
+     */
+    public void setRow(int rowIdx, double[] row) {
+        contents[rowIdx] = row;
+    }
+
+    /**
      * 
      * @param colIdx indeks kolom yang ingin diakses
      * @return kolom indeks ke-colIdx
@@ -60,6 +76,27 @@ public class Matrix {
             col[i] = contents[i][colIdx];
         }
         return col;
+    }
+
+    /**
+     * 
+     * @return konten matriks
+     */
+    public double[][] getContents() {
+        return contents;
+    }
+
+    /**
+     * Mengubah keseluruhan matriks
+     * <p>
+     * I.S. matriks terdefinisi
+     * <p>
+     * F.S. matriks berubah sesuai dengan contents
+     * 
+     * @param contents isi konten matriks baru
+     */
+    public void setContents(double[][] contents) {
+        this.contents = contents;
     }
 
     /**
@@ -84,20 +121,20 @@ public class Matrix {
      * @param colIdx indeks kolom yang ingin diubah
      * @param val    nilai baru
      */
-    public void setElmt(int rowIdx, int colIdx, int val) {
+    public void setElmt(int rowIdx, int colIdx, double val) {
         contents[rowIdx][colIdx] = val;
     }
 
     /*** PENGECEKAN MATRIKS ***/
     /**
-     * Mengecek apakah matriks merupakan matriks SPL yang valid (berukuran n x
-     * (n+1))
+     * Mengecek apakah matriks merupakan matriks SPL yang valid (minimal baris
+     * sejumlah kolom - 1)
      * 
      * @return true jika matriks merupakan matriks SPL yang valid, false jika tidak
      */
     public boolean isSPL() {
         // ALGORITMA
-        return getNCol() == getNRow() + 1;
+        return getNRow() >= getNCol() - 1;
     }
 
     /**
@@ -202,5 +239,122 @@ public class Matrix {
         for (int i = 0; i < getNRow(); i++) {
             multiplyRowScalar(i, scalar);
         }
+    }
+
+    /*** OPERATOR LAINNYA ***/
+
+    /**
+     * Mendapatkan kofaktor entri pada indeks (rowIdx, colIdx)
+     * 
+     * @param rowIdx
+     * @param colIdx
+     * @return matriks dengan baris ke rowIdx dan kolom ke colIdx dihilangkan
+     */
+    public Matrix getCofactorEntry(int rowIdx, int colIdx) {
+        // KAMUS LOKAL
+        Matrix res;
+        double multiplier;
+        int iRes, jRes;
+
+        // ALGORITMA
+        res = new Matrix(getNRow() - 1, getNCol() - 1);
+        multiplier = Math.pow(-1, rowIdx + colIdx);
+
+        for (int i = 0; i < getNRow(); i++) {
+            for (int j = 0; j < getNCol(); j++) {
+                if (i != rowIdx && j != colIdx) {
+                    // menentukan indeks baru
+                    iRes = i < rowIdx ? i : i - 1;
+                    jRes = j < colIdx ? j : j - 1;
+
+                    res.setElmt(iRes, jRes, getElmt(i, j) * multiplier);
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Mengubah matriks menjadi matriks SPL yang valid dengan membuat baris baru
+     * dengan elemen 0 hingga jumlah baris sama dengan jumlah kolom - 1
+     * <p>
+     * I.S. matriks terdefinisi
+     * <p>
+     * F.S. matriks merupakan matriks SPL yang valid
+     */
+    public void toValidSPL() {
+        // KAMUS LOKAL
+        Matrix temp;
+
+        // ALGORITMA
+        if (isSPL()) {
+            return;
+        }
+
+        temp = new Matrix(getNCol() - 1, getNCol());
+        for (int i = 0; i < getNRow(); i++) {
+            if (i < getNRow()) {
+                temp.setRow(i, getRow(i));
+            } else {
+                for (int j = 0; j < getNCol(); j++) {
+                    temp.setElmt(i, j, 0);
+                }
+            }
+        }
+
+        setContents(temp.getContents());
+    }
+
+    /**
+     * Mencari determinan matriks dengan metode kofaktor
+     * 
+     * @return determinan matriks
+     */
+    public double getDeterminantCofactor() {
+        // KAMUS LOKAL
+        double res;
+
+        // ALGORITMA
+        // basis, matriks 1x1
+        if (getNRow() == 1) {
+            return getElmt(0, 0);
+        } else {
+            res = 0;
+            for (int i = 0; i < getNCol(); i++) {
+                res += getElmt(0, i) * getCofactorEntry(0, i).getDeterminantCofactor();
+            }
+            return res;
+        }
+    }
+
+    /**
+     * Mencari solusi SPL dengan metode cramer
+     * 
+     * @return matriks solusi SPL (kolom berjumlah 1)
+     */
+    public Matrix getSolCramer() {
+        // KAMUS LOKAL
+        Matrix res, temp;
+        double det;
+
+        // ALGORITMA
+        res = new Matrix(getNCol() - 1, 1);
+        det = getDeterminantCofactor();
+
+        for (int i = 0; i < getNCol() - 1; i++) {
+            temp = new Matrix(getNRow(), getNCol());
+            for (int j = 0; j < getNRow(); j++) {
+                for (int k = 0; k < getNCol(); k++) {
+                    if (k == i) {
+                        temp.setElmt(j, k, getElmt(j, getNCol() - 1));
+                    } else {
+                        temp.setElmt(j, k, getElmt(j, k));
+                    }
+                }
+            }
+            res.setElmt(i, 0, temp.getDeterminantCofactor() / det);
+        }
+
+        return res;
     }
 }
