24709a806e6977734dc74b4615198f6de9a8c2b4
Fatih20 <fnri39@protonmail.com>
2022-09-21 10:16:49 +0700

    Refactoring of FromKeyboard.

--- src/lib/FromKeyboard.java
+++ src/lib/FromKeyboard.java
@@ -5,127 +5,200 @@ import java.util.Scanner; // Import the Scanner class
 
 public class FromKeyboard {
 
-    static private Matrix Matrix(int nRow, int nCol, String message) {
+    /**
+     * Fungsi baca angka generik. Membaca sebuah angka yang dimasukkan user.
+     * Mengulangi input jika angka yang dimasukkan berada di luar range yang
+     * diberikan
+     * 
+     * @param numberName nama angka yang dibaca. Teks digunakan untuk user prompt.
+     * @param lowerBound batas minimum angka yang boleh
+     * @param upperBound batas maksimum angka yang boleh
+     * @return angka masukan pengguna yang sudah sesuai dengan keperluan
+     */
+    static private int readNumber(String numberName, double lowerBound, double upperBound) {
         Scanner inputReceiver = new Scanner(System.in);
+        int inputtedNumber = 0;
+        boolean inputValid = false;
+        while (!inputValid) {
+            System.out.format("Enter %s : ", numberName);
+            inputtedNumber = inputReceiver.nextInt();
+            inputValid = inputtedNumber >= lowerBound && inputtedNumber <= upperBound;
+            if (!inputValid) {
+                System.out.format("Invalid %s, please try again", numberName);
+            }
+
+        }
+
+        return inputtedNumber;
+    }
+
+    /**
+     * Fungsi baca angka generik. Membaca sebuah angka yang dimasukkan user.
+     * Mengulangi input jika angka yang dimasukkan berada di bawah lowerBound yang
+     * diberikan
+     * 
+     * @param numberName nama angka yang dibaca. Teks digunakan untuk user prompt.
+     * @param lowerBound batas minimum angka yang boleh
+     * @return angka masukan pengguna yang sudah sesuai dengan keperluan
+     */
+    static private int readNumberWithMinimum(String numberName, int lowerBound) {
+        return readNumber(numberName, lowerBound, Double.MAX_VALUE);
+    }
+
+    /**
+     * Fungsi baca angka generik. Membaca sebuah angka yang dimasukkan user.
+     * Mengulangi input jika angka yang dimasukkan berada di atas upperBound yang
+     * diberikan
+     * 
+     * @param numberName nama angka yang dibaca. Teks digunakan untuk user prompt.
+     * @param upperBound batas maksimum angka yang boleh
+     * @return angka masukan pengguna yang sudah sesuai dengan keperluan
+     */
+    // static private int readNumberWithMaximum(String numberName, int upperBound) {
+    // return readNumber(numberName, Double.MIN_VALUE, upperBound);
+
+    // }
+
+    /**
+     * Fungsi baca baris generik. Membaca satu baris angka double dengan tiap elemen
+     * dipisahkan spasi.
+     * Mengulangi input jika kolom yang dimasukkan jumlahnya tidak
+     * sesuai atau ada elemen yang bukan angka
+     * 
+     * @param nCol jumlah kolom yang ingin dibaca
+     * @return matriks berisi double sepanjanng nCol yang nilainya didapatkan dari
+     *         masukan pengguna
+     */
+    static private double[] readRow(int nCol) {
+        Scanner inputReceiver = new Scanner(System.in);
+        double[] rowArrayTest = new double[0];
+        boolean rowInputValid = false;
+        boolean inputNotANumber = false;
+        boolean inputNotNRow = false;
+        while (!rowInputValid) {
+            String errorMessage = "";
+            System.out.printf("");
+            String rowString = inputReceiver.nextLine();
+            String[] rowArrayRaw = rowString.split(" ");
+            inputNotNRow = rowArrayRaw.length != nCol;
+            try {
+                rowArrayTest = Arrays.stream(rowArrayRaw).mapToDouble(Double::parseDouble).toArray();
+            } catch (NumberFormatException e) {
+                inputNotANumber = false;
+            }
+
+            if (inputNotNRow) {
+                errorMessage = "Length of the row is not the number of matrix column.";
+            }
+
+            if (inputNotANumber) {
+                errorMessage = "The inputted row contains not a valid number.";
+            }
+
+            rowInputValid = !inputNotANumber && !inputNotNRow;
+
+            if (!rowInputValid) {
+                System.out.printf("%s Please try again%n", errorMessage);
+            }
+        }
+
+        return rowArrayTest;
+    }
+
+    /**
+     * Fungsi baca matriks generik. Membaca matriks baris per baris dengan elemen
+     * antara kolom dipisahkan spasi.
+     * Mengulangi input salah satu baris jika kolom yang dimasukkan jumlahnya tidak
+     * sesuai atau ada elemen yang bukan angka
+     * 
+     * @param nRow jumlah baris yang ingin dibaca
+     * @param nCol jumlah kolom yang ingin dibaca
+     * @return matriks baru dengan ukuran nRow x nCol yang nilainya didapatkan dari
+     *         masukan pengguna
+     */
+    static private Matrix Matrix(int nRow, int nCol, String message) {
         Matrix inputtedMatrix = new Matrix(nRow, nCol);
         System.out.println(message);
 
         int i;
         for (i = 0; i < nRow; i++) {
-
-            boolean rowInputValid = false;
-            boolean inputNotANumber = false;
-            boolean inputNotNRow = false;
-            while (!rowInputValid) {
-                String errorMessage = "";
-                System.out.printf("");
-                String rowString = inputReceiver.nextLine();
-                String[] rowArrayRaw = rowString.split(" ");
-                double[] rowArrayTest = new double[0];
-                inputNotNRow = rowArrayRaw.length != nCol;
-                try {
-                    rowArrayTest = Arrays.stream(rowArrayRaw).mapToDouble(Double::parseDouble).toArray();
-                } catch (NumberFormatException e) {
-                    inputNotANumber = false;
-                }
-
-                if (inputNotNRow) {
-                    errorMessage = "Length of the row is not the number of matrix column.";
-                }
-
-                if (inputNotANumber) {
-                    errorMessage = "The inputted row contains not a valid number.";
-                }
-
-                rowInputValid = !inputNotANumber && !inputNotNRow;
-
-                if (!rowInputValid) {
-                    System.out.printf("%s Please try again%n", errorMessage);
-                } else {
-                    double[] rowArray = rowArrayTest.clone();
-                    inputtedMatrix.setRow(i, rowArray);
-                }
-            }
-
+            double[] row = readRow(nCol);
+            inputtedMatrix.setRow(i, row);
         }
-
-        // inputReceiver.close();
         return inputtedMatrix;
-    }
-
-    static public Matrix MatrixOrSPL() {
-
-        Scanner inputReceiver = new Scanner(System.in);
-        int m = 0;
-        boolean inputMValid = false;
-        while (!inputMValid) {
-            System.out.print("Enter m : ");
-            m = inputReceiver.nextInt();
-            inputMValid = m > 0;
-            if (!inputMValid) {
-                System.out.println("Invalid m, please try again");
-            }
-
-        }
-
-        int n = 0;
-        boolean inputNValid = false;
-        while (!inputNValid) {
-            System.out.print("Enter n : ");
-            n = inputReceiver.nextInt();
-            inputNValid = n > 0;
-            if (!inputNValid) {
-                System.out.println("Invalid n, please try again");
-
-            }
-        }
-        inputReceiver.nextLine();
-
-        // inputReceiver.close();
-
-        return Matrix(m, n, "Enter your matrix row by row: ");
 
     }
 
+    /**
+     * Fungsi baca matriks berbentuk persegi. Membaca jumlah baris yang diinginkan,
+     * lalu membaca matriks baris per baris dengan elemen
+     * antara kolom dipisahkan spasi.
+     * Mengulangi input jumlah baris jika nilainya < 1.
+     * Mengulangi input salah satu baris jika kolom yang dimasukkan jumlahnya tidak
+     * sesuai atau ada elemen yang bukan angka
+     * 
+     * @return matriks baru dengan ukuran persegi yang sesuai dengan masukan
+     *         pengguna
+     */
     static public Matrix MatrixSquare() {
+        int n = readNumberWithMinimum("jumlah baris", 1);
 
-        Scanner inputReceiver = new Scanner(System.in);
-
-        int n = 0;
-        boolean inputNValid = false;
-        while (!inputNValid) {
-            System.out.print("Enter n : ");
-            n = inputReceiver.nextInt();
-            inputNValid = n > 0;
-            if (!inputNValid) {
-                System.out.println("Invalid n, please try again");
-
-            }
-        }
-        inputReceiver.nextLine();
-
-        // inputReceiver.close();
-
-        return Matrix(2, 2, "Enter your matrix row by row: ");
+        return Matrix(n, n, "Masukkan matriks baris per baris dengan tiap elemen dipisahkan spasi");
     }
 
+    /**
+     * Fungsi baca kumpulan titik. Membaca jumlah titik yang ingin dibaca lalu
+     * membaca tiap titik satu per satu dengan x dan y dipisahkan spasi
+     * Mengulangi input jumlah baris jika nilainya < 1.
+     * Mengulangi input salah satu baris jika kolom yang dimasukkan jumlahnya tidak
+     * sesuai atau ada elemen yang bukan angka
+     * 
+     * @return matriks dengan 2 kolom. kolom pertamanya adalah x dan kolom keduanya
+     *         adalah y
+     */
     static public Matrix Points() {
-        Scanner inputReceiver = new Scanner(System.in);
+        int n = readNumberWithMinimum("jumlah variabel", 1);
 
-        int n = 0;
-        boolean inputNValid = false;
-        while (!inputNValid) {
-            System.out.print("Enter n : ");
-            n = inputReceiver.nextInt();
-            inputNValid = n > 0;
-            if (!inputNValid) {
-                System.out.println("Invalid n, please try again");
-
-            }
-        }
-        inputReceiver.nextLine();
-
-        return Matrix(n, 2, "Enter your points by row with x and y separated by space");
+        return Matrix(n, 2, "Masukkan titik baris per baris dengan x dan y dipisahkan spasi");
     }
 
+    /**
+     * Fungsi baca kumpulan persamaan. Meminta input jumlah persamaan dan jumlah
+     * variabel dari pengguna. Mengulangi kedua input tersebut jika < 1.
+     * Mengulangi input salah satu persaman jika elemen yang dimasukkan jumlahnya
+     * tidak
+     * sesuai atau ada elemen yang bukan angka
+     * 
+     * @return matriks dengan jumlah persamaan x (jumlah variabel + 1). Kolom
+     *         terakhir adalah nilai b sementara kolom lainnya adalah koefisien
+     *         variabel pada persamana
+     */
+    static public Matrix SPL() {
+        int m = readNumberWithMinimum("jumlah persamaan", 1);
+        int n = readNumberWithMinimum("jumlah variabel", 1);
+
+        return Matrix(m, n + 1,
+                "Masukkan SPL baris per baris dengan koefisien variabel dipisahkan spasi dan elemen terakhir sebagai b");
+
+    }
+
+    /**
+     * Fungsi baca matriks. Meminta input jumlah baris dan kolom dari pengguna.
+     * Mengulangi kedua input tersebut jika jumlah baris atau jumlah kolom < 1.
+     * Mengulangi input salah satu baris jika elemen yang dimasukkan jumlahnya
+     * tidak
+     * sesuai atau ada elemen yang bukan angka
+     * 
+     * @return matriks dengan jumlah persamaan x (jumlah variabel + 1). Kolom
+     *         terakhir adalah nilai b sementara kolom lainnya adalah koefisien
+     *         variabel pada persamana
+     */
+    static public Matrix Matrix() {
+        int m = readNumberWithMinimum("jumlah baris", 1);
+        int n = readNumberWithMinimum("jumlah kolom", 1);
+
+        return Matrix(m, n,
+                "Masukkan matriks baris per baris dengan tiap elemen dipisahkan spasi");
+
+    }
 }
