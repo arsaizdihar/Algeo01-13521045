fe134750d85cbba535c2b780ddc8250af125ae93
arsaizdihar <arsadihar@gmail.com>
2022-10-01 21:05:20 +0700

    Remove comment

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 import lib.ui.Interface;
-//TODO Inget yang determinan dengan metode segitiga atas blm
+
 public class Main {
     public static void main(String[] args) {
         Interface.mainEventLoop();
     }
 }
-// 1 2 3 4
-// 5 6 7 8
-// 9 10 11 12
-// 13 14 15 16
